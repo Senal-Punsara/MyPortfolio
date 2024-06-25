8b99ccf3626031520bbab1250165cda9daa2eff5
Senal-Punsara <kksenalpunsara@gmail.com>
2024-06-25 14:47:34 +0530

    add award section

---
+++ src/data/awards.js
@@ -0,0 +1,36 @@
+export const awards = [
+  {
+    id:4,
+    achivment: "1st Runners Up",
+    date:"May 2023",
+    organizer:"Inter Faculty Chess Championship - University of Colombo",
+    credential:"https://drive.google.com/file/d/11C2RdKhi4Hpd619vgW_ip0Zr2RausSp0/view"
+  },
+  {
+    id:3,
+    achivment: "2nd Runners Up",
+    date:"Nov 2022",
+    organizer:"Inter Faculty Chess Championship - University of Colombo",
+    credential:"https://drive.google.com/file/d/1AV2n-XY2wkl80sYTp7HjM4T0HZhL88KM/view"
+  },
+  {id:2,
+    achivment: "School Colors for Scouting",
+    date:"Apr 2019",
+    organizer:"Nalanda College, Colombo 10",
+    credential:"https://drive.google.com/file/d/1kAsbi0xON-WebP_3IIi29FLB3D-p_rbZ/view"
+  },
+  {
+    id:1,
+    achivment: "President's Scout Award",
+    date:"Jan 2017",
+    organizer:"Sri Lanka Scout Association",
+    credential:"https://drive.google.com/file/d/1ViMKeqnIkCSSjFVUSSme5vZevWhAq-RA/view"
+  },
+  {
+    id:0,
+    achivment: "Champions",
+    date:"Jan 2017",
+    organizer:"All Island Inter School Team Chess Championship - Under 14",
+    credential:"https://drive.google.com/file/d/1xfM11tKV3KLtO7Sayp9vjtI__P5UasJH/view"
+  },
+];
